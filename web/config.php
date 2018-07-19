6b8e4e3abf87fe2018f28064e0ea3e3f35abdfb1
luolikongxiaowen <41230998+luolikongxiaowen@users.noreply.github.com>
2018-07-19 18:22:18 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,6 +1,6 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY',AIzaSyCn9ZafIaoKypjVCIG-utCam4JubK3T36U("APIKEY"));
+define('APIKEY',AIzaSyCn9ZafIaoKypjVCIG-utCam4JubK3T36U("AIzaSyCn9ZafIaoKypjVCIG-utCam4JubK3T36U"));
 define('GJ_CODE', getenv("GJ_CODE"));
 define('SITE_NAME',getenv("SITE_NAME"));
 define('TITLENAME', getenv("TITLENAME"));
