7fb656aa10a9c1398efe90592a5dd733d41d3f02
luolikongxiaowen <41230998+luolikongxiaowen@users.noreply.github.com>
2018-07-19 18:24:47 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY',AIzaSyCn9ZafIaoKypjVCIG-utCam4JubK3T36U("AIzaSyCn9ZafIaoKypjVCIG-utCam4JubK3T36U"));
-define('GJ_CODE', getenv("GJ_CODE"));
-define('SITE_NAME',getenv("SITE_NAME"));
-define('TITLENAME', getenv("TITLENAME"));
-define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+define('APIKEY', 'AIzaSyCn9ZafIaoKypjVCIG-utCam4JubK3T36U');//这里改成您的api key
+define('GJ_CODE', 'TW');//国家代码
+define('SITE_NAME', 'luolikongxiaowen');//网站名字
+define('TITLENAME', 'luolikongxiaowen');//网站名字
+define('EN2DEKEY', 'AnEUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
+define('EMAIL', '1040114823@qq.com');//邮箱
 ?>
